ab9285382ec64c4e1ea912932588a439c3de69f8
Vagner de Souza <inaciovagner1@gmail.com>
2021-05-14 15:49:53 +0100

    Add project files.

---
+++ Understanding_constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Understanding_constructor
+{
+    class Calc
+    {
+        public int result = 0; 
+        static void Main(string[] args)
+        {
+            Calc square = new Calc(3);
+            Calc mul = new Calc(4, 5);
+            Console.WriteLine(mul.result + square.result);
+        }
+        // Modify the constructor below //
+        public Calc(int a)
+        {
+            this.result = a * a;
+        }
+        public Calc(int a, int b)
+        {
+            this.result = a * b;
+        }
+    }
+}
